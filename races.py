7f6e5fb05c844f326a07401025dde4aa4b3bd470
LucioBn <lucio.benca@mail.polimi.it>
2022-10-10 18:19:56 +0200

    Added races.py

---
+++ races.py
@@ -0,0 +1,47 @@
+races = {
+    'Human': {
+        'ability_score': ['Str', 'Con', 'Dex', 'Int', 'Wis', 'Cha'],
+        'small': False,
+        'racial power': 'Quick to Fight'
+    },
+    'Dwarf': {
+        'ability_score': ['Con', 'Wis'],
+        'small': False,
+        'racial power': 'That’s Your Best Shot?'
+    },
+    'Half-orc': {
+        'ability_score': ['Str', 'Dex'],
+        'small': False,
+        'racial power': 'Lethal'
+    },
+    'Dark Elf': {
+        'ability_score': ['Dex', 'Cha'],
+        'small': False,
+        'racial power': 'Cruel',
+    },
+    'High Elf': {
+        'ability_score': ['Int', 'Cha'],
+        'small': False,
+        'racial power': 'Highblood teleport',
+    },
+    'Wood Elf': {
+        'ability_score': ['Dex', 'Wis'],
+        'small': False,
+        'racial power': 'Elven Grace',
+    },
+    'Gnome': {
+        'ability_score': ['Int', 'Dex'],
+        'small': True,
+        'racial power': 'Confounding'
+    },
+    'Half-elf': {
+        'ability_score': ['Con', 'Cha'],
+        'small': False,
+        'racial power': 'Surprising',
+    },
+    'Halfling': {
+        'ability_score': ['Con', 'Dex'],
+        'small': True,
+        'racial power': 'Evasive'
+    }
+}
