323bc30c1c34abb2edd5a05412cf40438f45290f
LucioBn <lucio.benca@mail.polimi.it>
2022-10-22 20:50:49 +0200

    icons now is a dict

--- icons.py
+++ icons.py
@@ -1,1 +1,67 @@
-icons = ['Archmage', 'Crusader', 'Diabolist', 'Dwarf King', 'Elf Queen', 'Emperor', 'Great Gold Wyrm', 'High Druid', 'Lich King', 'Orc Lord', 'Priestess', 'Prince of Shadows', 'The Three']
+icons = {
+    'Archmage': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 1
+    },
+    'Crusader': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 2
+    },
+    'Diabolist': {
+        'Positive_relationship_max_value': 1,
+        'Conflicted_relationship_max_value': 2,
+        'Negative_relationship_max_value': 2
+    },
+    'Dwarf King': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 2
+    },
+    'Elf Queen': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 2
+    },
+    'Emperor': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 1
+    },
+    'Great Gold Wyrm': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 1
+    },
+    'High Druid': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 2
+    },
+    'Lich King': {
+        'Positive_relationship_max_value': 1,
+        'Conflicted_relationship_max_value': 2,
+        'Negative_relationship_max_value': 2
+    },
+    'Orc Lord': {
+        'Positive_relationship_max_value': 1,
+        'Conflicted_relationship_max_value': 2,
+        'Negative_relationship_max_value': 2
+    },
+    'Priestess': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 1
+    }, 
+    'Prince of Shadows': {
+        'Positive_relationship_max_value': 3,
+        'Conflicted_relationship_max_value': 3,
+        'Negative_relationship_max_value': 2
+    },
+    'The Three': {
+        'Positive_relationship_max_value': 1,
+        'Conflicted_relationship_max_value': 2,
+        'Negative_relationship_max_value': 2
+    }
+}
