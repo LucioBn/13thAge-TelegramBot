f70068f904c76c0e77f87ec0317d798b6dba2ae8
LucioBn <lucio.benca@mail.polimi.it>
2022-11-02 11:58:33 +0100

    fix

---
+++ equipment.py
@@ -0,0 +1,576 @@
+equipment = {
+    "Standard Traveling Gear": {
+        "Flint and tinder box": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Money pouch (small)": {
+            "value": 3,
+            "coin": "sp"
+        },
+        "Pack/traveling satchel": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Rain cloak": {
+            "value": 3,
+            "coin": "sp"
+        },
+        "Road rations (5 days)": {
+            "value": 25,
+            "coin": "sp"
+        },
+        "Sleeping roll": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Water/wine skin or flask": {
+            "value": 7,
+            "coin": "sp"
+        }
+    },
+    "General Goods": {
+        "Arrows": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Bolts": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Sling bullet": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Blanket (wool)": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Candle": {
+            "value": 1,
+            "coin": "cp"
+        },
+        "Chain, dwarven forged (10 ft)": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Chain, iron (10 ft)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Clothing, simple": {
+            "value": 2,
+            "coin": "sp"
+        },
+        "Clothing, good": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Clothing, expensive": {
+            "value": 55,
+            "coin": "gp"
+        },
+        "Clothing, rain cloak (elven)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Crowbar (iron)": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Flask (crystal)": {
+            "value": 54,
+            "coin": "gp"
+        },
+        "Flask (glass)": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Flask (pottery)": {
+            "value": 5,
+            "coin": "cp"
+        },
+        "Grappling hook (iron)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Hammer, small": {
+            "value": 3,
+            "coin": "sp"
+        },
+        "Holy symbol/implement (adv)": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Holy symbol/implement (chp)": {
+            "value": 100,
+            "coin": "gp"
+        },
+        "Holy symbol/implement (epic)": {
+            "value": 1000,
+            "coin": "gp"
+        },
+        "Iron spike": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Lantern (common)": {
+            "value": 8,
+            "coin": "sp"
+        },
+        "Lantern (hooded)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Lantern oil (4 hrs)": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Magnifying glass": {
+            "value": 25,
+            "coin": "gp"
+        },
+        "Mirror, large": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Mirror, small": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Musical instrument (intricate)": {
+            "value": 15,
+            "coin": "gp"
+        },
+        "Musical instrument (simple)": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Pipeweed (1 use)": {
+            "value": 2,
+            "coin": "cp"
+        },
+        "Prayer book": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Rope, 50 ft elven": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Rope, 50 ft good": {
+            "value": 6,
+            "coin": "sp"
+        },
+        "Rope, 50 ft poor": {
+            "value": 2,
+            "coin": "sp"
+        },
+        "Spellbook (adventurer)": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Spellbook (champion)": {
+            "value": 100,
+            "coin": "gp"
+        },
+        "Spellbook (epic)": {
+            "value": 1000,
+            "coin": "gp"
+        },
+        "Tent, large": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Tent, small": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Tent, wood elven": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Thieves’ tools (adventurer)": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Thieves’ tools (champion)": {
+            "value": 20,
+            "coin": "gp"
+        },
+        "Thieves’ tools (epic)": {
+            "value": 200,
+            "coin": "gp"
+        },
+        "Torch (1 hr.)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Torch (6 min)": {
+            "value": 1,
+            "coin": "sp"
+        }
+    },
+    "Food/Lodging": {
+        "Road rations (1 day)": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Meal, common": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Meal, good": {
+            "value": 3,
+            "coin": "sp"
+        },
+        "Meal, excellent": {
+            "value": 8,
+            "coin": "sp"
+        },
+        "Meal, feast (for 5)": {
+            "value": 8,
+            "coin": "gp"
+        },
+        "Ale/beer, poor (pitcher)": {
+            "value": 2,
+            "coin": "cp"
+        },
+        "Ale/beer, good (pitcher)": {
+            "value": 6,
+            "coin": "cp"
+        },
+        "Ale/beer, dwarven (pitcher)": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Wine, poor (bottle)": {
+            "value": 4,
+            "coin": "cp"
+        },
+        "Wine, good (bottle)": {
+            "value": 1,
+            "coin": "sp"
+        },
+        "Wine, elven (bottle)": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Inn (per person per day)": {
+            "value": 0,
+            "coin": "sp"
+        },
+        "Poor, common room": {
+            "value": 2,
+            "coin": "sp"
+        },
+        "Good, shared room": {
+            "value": 11,
+            "coin": "sp"
+        },
+        "Excellent, private room": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Suite (sleeps 4 to 8)": {
+            "value": 13,
+            "coin": "gp"
+        }
+    },
+    "Mounts (including gear)": {
+        "Dog, guard": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Dog, riding": {
+            "value": 15,
+            "coin": "gp"
+        },
+        "Donkey/pack mule": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Horse, riding": {
+            "value": 20,
+            "coin": "gp"
+        },
+        "Horse, battle trained": {
+            "value": 80,
+            "coin": "gp"
+        },
+        "Feed for mount (per day)": {
+            "value": 3,
+            "coin": "cp"
+        }
+    },
+    "Weapons, Melee (average quality)": {
+        "Battle gauntlets": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Battleaxe": {
+            "value": 6,
+            "coin": "gp"
+        },
+        "Club": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Dagger/knife": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Double axe": {
+            "value": 12,
+            "coin": "gp"
+        },
+        "Flail": {
+            "value": 6,
+            "coin": "gp"
+        },
+        "Greataxe": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Greatsword": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Handaxe": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Longsword": {
+            "value": 7,
+            "coin": "gp"
+        },
+        "Mace": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Maul": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Morningstar": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Pick": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Polearm": {
+            "value": 8,
+            "coin": "gp"
+        },
+        "Rapier": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Sap": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Scimitar": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Shortsword": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Spear": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Staff": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Trident": {
+            "value": 4,
+            "coin": "gp"
+        },
+        "Two-bladed sword": {
+            "value": 15,
+            "coin": "gp"
+        },
+        "Warhammer": {
+            "value": 7,
+            "coin": "gp"
+        }
+    },
+    "Weapons, Ranged (average quality)": {
+        "Bow: Longbow": {
+            "value": 14,
+            "coin": "gp"
+        },
+        "Bow: Shortbow": {
+            "value": 9,
+            "coin": "gp"
+        },
+        "Crossbow: Hand crossbow": {
+            "value": 8,
+            "coin": "gp"
+        },
+        "Crossbow: Light crossbow": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Crossbow: Heavy crossbow": {
+            "value": 15,
+            "coin": "gp"
+        },
+        "Javelin": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Net, weighted": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Sling": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Shuriken": {
+            "value": 7,
+            "coin": "sp"
+        },
+        "Throwing axe": {
+            "value": 3,
+            "coin": "gp"
+        },
+        "Throwing hammer": {
+            "value": 3,
+            "coin": "gp"
+        }
+    },
+    "Armor (average quality)": {
+        "Cloth/padded": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Leather": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Studded leather": {
+            "value": 15,
+            "coin": "gp"
+        },
+        "Hide": {
+            "value": 10,
+            "coin": "gp"
+        },
+        "Light chain": {
+            "value": 20,
+            "coin": "gp"
+        },
+        "Heavy chain/ring": {
+            "value": 30,
+            "coin": "gp"
+        },
+        "Scale": {
+            "value": 40,
+            "coin": "gp"
+        },
+        "Half-plate": {
+            "value": 45,
+            "coin": "gp"
+        },
+        "Plate": {
+            "value": 50,
+            "coin": "gp"
+        },
+        "Shield": {
+            "value": 5,
+            "coin": "gp"
+        }
+    },
+    "Services": {
+        "Bath, with servants": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Bodyguard (d)": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Carriage/coach, one ride": {
+            "value": 2,
+            "coin": "sp"
+        },
+        "Carriage/coach": {
+            "value": 2,
+            "coin": "gp"
+        },
+        "Courier (intercity)": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Courier (long distance)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Courier (urchin)": {
+            "value": 5,
+            "coin": "cp"
+        },
+        "Courtesan": {
+            "value": 50,
+            "coin": "gp"
+        },
+        "Guide, urban (d)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Guide, wilderness (d)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Herald (in city only)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Lantern bearer (in city) (d)": {
+            "value": 5,
+            "coin": "sp"
+        },
+        "Mercenary, soldier (d)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Mercenary, thug (d)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Moneylender (cost per 100gp)": {
+            "value": 5,
+            "coin": "gp"
+        },
+        "Oracle/seer/fortune teller": {
+            "value": 25,
+            "coin": "gp"
+        },
+        "Personal chef (d)": {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Sage": {
+            "value": 28,
+            "coin": "gp"
+        },
+        "Valet/manual laborer":  {
+            "value": 1,
+            "coin": "gp"
+        },
+        "Cleric (divine spells/rituals)": {
+            "value": "varies"
+        },
+        "Wizard (arcane spells/rituals)": {
+            "value": "varies"
+        }
+    }
+}
